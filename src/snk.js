89ee2fa8e0ea58c248738e882b0caf9e5e46aef4
ramadhankukuh <73922679+ramadhankukuh@users.noreply.github.com>
2021-01-02 13:11:22 +0700

    Update snk.js

--- src/snk.js
+++ src/snk.js
@@ -1,5 +1,5 @@
 const snk = (prefix) => { 
-	return `Syarat dan Ketentuan Bot *XPTN* 
+	return `Syarat dan Ketentuan Bot *RKBOT* 
 1. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
 2. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
 3. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
